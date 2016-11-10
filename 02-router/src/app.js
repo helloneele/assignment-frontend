4c852c26426f01cbefd4c2c65783b7a1ed827638
helloneele <hello@neele.codes>
2016-11-10 14:25:29 +0100

    working in chrome

--- 02-router/src/app.js
+++ 02-router/src/app.js
@@ -2,12 +2,17 @@ import $ from 'jquery'
 import router from './router'
 import homeTpl from './templates/home.hbs'
 import contactTpl from './templates/contact.hbs'
+import playerTpl from './templates/player.hbs'
 import notFoundTpl from './templates/not-found.hbs'
 
 const $app = $('#app')
 
 function index() {
   $app.html(homeTpl())
+}
+
+function player() {
+  $app.html(playerTpl())
 }
 
 function contact() {
@@ -19,6 +24,7 @@ function notFound() {
 }
 
 router('/', index)
+router('/players/:player', player)
 router('/contact', contact)
 router('*', notFound)
 router()
--- 02-router/src/router.js
+++ 02-router/src/router.js
@@ -1,3 +1,95 @@
-export default function() {
+import $ from 'jquery'
 
+const routes = new Map()
+
+function init(){
+  const path = window.location.pathname //gets uri of current page
+
+  goto(path)
+}
+
+function goto(route){
+  // for (const [route, val] of)
+  // routes.entries()){
+  //   if (route.match(regEXP))
+  // }
+  console.log("route: " + route);
+  //console.log(event);
+
+
+
+  //window.history.state.pathname //gets name of back
+  //console.log(window.history.state.pathname);
+  console.log(window.history.length);
+  let lastViewed;
+  if(window.history.state.pathname)
+    lastViewed = window.history.state.pathname;
+  else
+    lastViewed = false
+
+  // check if event is popstate and if last page visited is the same as current page
+  if(event && event.type != "popstate" && route != lastViewed){
+    console.log("hi");
+    window.history.pushState({
+      pathname: route
+    }, '', route);
+  }
+
+
+  console.log(window.history)
+
+
+  for ( let [key, val] of routes.entries() ) {
+    //console.log("key: " + key + " route: " + route + " value: " + val);
+    if (key === route) return val();
+  }
+
+  if (routes.has('*')) {
+    return routes.get('*')();
+  }
+
+
+}
+
+export default function(route, fn) {
+  //console.log(route, fn)
+  // register new route
+  if(route && fn){
+    routes.set(route, fn)
+    return
+  }
+
+  // init if no params
+  if(!route && !fn){
+    //console.log(1);
+    initEventListeners();
+    init()
+  }
+
+
+}
+
+function initEventListeners () {
+
+  const nav = $('nav a');
+
+  for (var i = 0; i < nav.length; i++) {
+    nav[i].addEventListener('click', e => {
+      console.log(e);
+      e.preventDefault();
+      goto(e.srcElement.pathname)
+    });
+  }
+
+  window.onpopstate = function (e) {
+    goto(e.state.pathname);
+  }
+
+  window.addEventListener('popstate', e => {
+    //console.log("pathname" + e.state.pathname);
+    let state = e.state;
+    if (state !== null) {
+      goto(state.pathname);
+    }
+  })
 }
